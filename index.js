c1e04085574870e3300fbb729e5c522d1a2759c0
AmirHossein <Amirhosseinaghaee22@gmail.com>
2025-03-29 16:58:14 +0330

    :memo: add middlwar and private chat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const { Server } = require("socket.io");
+const e = require("express");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +13,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-
 app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
+
+app.get("/chat/:socket_id", (req, res) => {
   res.sendFile(__dirname + "/chat.html");
 });
 
@@ -22,36 +26,46 @@ server.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
 
-let users = [] ;
+let users = [];
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  const validToken = "1234";
+  if (token === validToken) next();
+  else console.log("token inValid");
+});
 io.on("connection", (socket) => {
   // console.log(`a user connected`);
 
-  socket.on("login" , (name) => {
+  socket.on("login", (name) => {
     const temp = name;
-    users = users.filter(user => user.id !== temp.id );
+    users = users.filter((user) => user.id !== temp.id);
     users.push({
-      ...temp ,
-      socket_id : socket.id ,
-    })
-    
-    io.emit("online_user" , users)
-  })
-  socket.on("disconnect", () => {
+      ...temp,
+      socket_id: socket.id,
+    });
 
-    const temp_socket_id = socket.id ;
-    users = users.filter(user => user.socket_id !== temp_socket_id );
-    io.emit("online_user" , users)
+    io.emit("online_user", users);
+  });
+  socket.on("disconnect", () => {
+    const temp_socket_id = socket.id;
+    users = users.filter((user) => user.socket_id !== temp_socket_id);
+    io.emit("online_user", users);
 
     console.log("user disconnected");
   });
 
   socket.on("chat message", (msg) => {
-    io.emit("chat message", { ...msg, user_id: socket.id });
+    if (msg?.to)
+      io.to(msg.to).emit("chat message", {
+        ...msg,
+        user_id: socket.id,
+        from: socket.id,
+      });
+    else io.emit("chat message", { ...msg, user_id: socket.id });
   });
 
   socket.on("typing", (data) => {
-    // send to all connection 
-    socket.broadcast.emit("typing" , data)
-  
+    // send to all connection
+    socket.broadcast.emit("typing", data);
   });
 });
